84002b50440a99aa57b6100651712eb79cfced6e
Alex Demidovich <aldemidovcich@gmail.com>
2022-02-07 01:03:27 +0300

    time 48:38

---
+++ app.js
@@ -0,0 +1,22 @@
+// 1 Переменные
+//variable
+//var name ='Vladilen' // старое, лучше не использовать
+const firstName = 'Vladilen'
+const lastName = 'Minin'// string
+const age = 26 //numder
+const isProgrammer = true // boolean
+
+
+
+
+const _ = 'private' // можно
+const $ = 'some value'  // можно
+
+//const if = 'mkef'// нельзя, ошибка
+
+const withNum5 = '5' // можно
+//const 5withNum = '5' //нельзя начинать название переменной с цифры, ошибка
+
+// 2 Мутирование
+//console.log('Имя человека: ' + firstName + ' ,а возраст человека: ' + age)
+console.log('Имя человека: ' + firstName + ' ,а возраст человека: ' + age)
